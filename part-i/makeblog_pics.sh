61610ddc1ac2c058bc65b28446c6c794000b2290
rabidgremlin <jack@rabidgremlin.com>
2014-12-23 14:14:36 +1300

    More tweaks.

--- part-i/makeblog_pics.sh
+++ part-i/makeblog_pics.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-convert GenerateExamples/anim/*.png -set delay 10 -reverse GenerateExamples/anim/*.png -set delay 10 -loop 0 -layers optimize animated_tree.gif
+mkdir resized
+convert GenerateExamples/anim/*.png -resize 512 resized/res%03d.png
+convert resized/*.png -set delay 10 -reverse resized/*.png -set delay 10 -loop 0 -layers optimize animated_tree.gif
+
 
 montage -mode concatenate -tile 2x GenerateExamples/st_*.png st_tiled.png
 montage -mode concatenate -tile 2x GenerateExamples/tree*.png tree_tiled.png
+montage -mode concatenate -tile 2x 3dExamples/*.png -resize 512 3d_examples.jpg
